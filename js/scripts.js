1a0d318eb6e2c68b540e5799373cbbf811eac17e
corey redding and tyler allen <corey@>
2017-05-01 11:00:12 -0700

    modify css index.html and scripts.js to create functional arrays after submitting information in the Index.html

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,11 @@ $(function() {
   var arrayOne = [dogBreed, favFood, favColor, favBand];
   //alert(arrayOne);
   console.log(arrayOne);
-  var arrayTwo = [];
 
-  arrayTwo.push(arrayOne[1]); //arrayTwo[0]
-  arrayTwo.push(arrayOne[0]); //arrayTwo[1]
-  arrayTwo.push(arrayOne[2]); //arrayTwo[2]
+  var arrayTwo = [arrayOne[1], arrayOne[0], arrayOne[2]];
+  // arrayTwo.push(arrayOne[1]); //arrayTwo[0]
+  // arrayTwo.push(arrayOne[0]); //arrayTwo[1]
+  // arrayTwo.push(arrayOne[2]); //arrayTwo[2]
 
   $(".output").show();
   $("#arrayTwo-0").text(arrayTwo[0]);
